b5738e81c3d3931bee96815885fb21f1f3508b47
Arjun Nair~ <arjunnair101105@gmail.com>
2025-02-15 01:17:56 +0530

    server.js fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://your-deployed-frontend.vercel.app"],
+  origin: ["http://localhost:3000", "https://ammas-kitchen.vercel.app/"],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
