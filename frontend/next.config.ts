e42c8df5bbc189f9ba2a537ec94f51a201aafa5d
Arjun Nair~ <arjunnair101105@gmail.com>
2025-02-15 00:53:06 +0530

    fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ✅ This replaces `next export`
 };
 
 export default nextConfig;
