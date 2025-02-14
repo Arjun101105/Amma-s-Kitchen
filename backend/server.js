6091e9859a36e3131d4ec24b56aa4b60db1b8016
Arjun Nair~ <arjunnair101105@gmail.com>
2025-02-15 00:07:59 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,10 @@ app.get("/", (req, res) => {
 });
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 
 
 // Export for Vercel (VERY IMPORTANT)
